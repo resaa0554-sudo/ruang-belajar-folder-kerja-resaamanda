8950e70b2537d7da2a5b27100ef50a5ddb780320
resaa0554-sudo <resaa0554@gmail.com>
2026-04-25 08:14:37 +0700

    Add files via upload

---
+++ loopbintang.java
@@ -0,0 +1,18 @@
+public class loopbintang {
+    public static void main(String[] args) {
+
+        int i = 1;
+
+        while (i <= 5) {
+            int j = 1;
+
+            while (j <= 5) {
+                System.out.print("* ");
+                j++;
+            }
+
+            System.out.print("\n");
+            i++;
+        }
+    }
+}
